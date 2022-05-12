f37ff365775496610d21babd352b5b695658f365
BlueBug12 <benson.liu.himself@gmail.com>
2022-05-12 16:34:39 +0800

    red light2

--- Potter.py
+++ Potter.py
@@ -1,3 +1,9 @@
 class Potter:
     def price(self, basket):
-        return 8 * len(basket)
+        max_combine = len(set(basket))
+        if max_combine == len(basket):
+            return self.discountprice(basket)
+        else:
+            return 8 * len(basket)
+    def discountprice(self, basket):
+        return 0
