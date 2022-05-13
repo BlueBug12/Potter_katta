2b259122462d6f1df2067761c74e3acfa0156716
BlueBug12 <benson.liu.himself@gmail.com>
2022-05-14 00:02:55 +0800

    green light 4

--- Potter.py
+++ Potter.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 class Potter:
     def __init__(self,book_price):
         self.book_price = book_price
@@ -8,16 +9,27 @@ class Potter:
         for b in books:
             basket.remove(b)
         return basket
-    def discountprice(self, basket):
-        type_num = len(basket)
-        if len(set(basket)) != type_num:
-            print('Error: only accept unique basket.')
-        return self.book_price*type_num*self.discount[type_num]
+    def discountprice(self, n):
+        return self.book_price*n*self.discount[n]
     def price(self, basket):
-        if len(set(basket)) == len(basket):
-            return self.discountprice(basket)
-        else:
-            max_combine = len(set(basket))
-            comb = self.pick_combination(basket, max_combine)
-            return self.discountprice(comb) + self.price(self.remove_books_from_basket(basket, comb))
-        
+        type_num = len(set(basket))
+        return self.count(basket,type_num)
+
+    def count(self,basket,num):
+        type_list = list(set(basket))
+        if len(type_list) == len(basket):
+            return self.discountprice(len(type_list))
+        if len(type_list) == 1:
+            return len(basket)*self.book_price
+        total_p = 1e9
+        for j in range(num,0,-1):
+            combs = combinations(type_list,j)
+            for comb in combs:
+                self.remove_books_from_basket(basket,comb)
+                total_p = min(self.count(basket,j)+self.discountprice(j),total_p)
+                basket += comb
+        return total_p
+
+if __name__ == "__main__":
+    p = Potter(8)
+    print(p.price([0,0,1,1]))
