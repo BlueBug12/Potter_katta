d866cf4209f9389a4ee3464ecd5a4f44c4e39d06
BlueBug12 <benson.liu.himself@gmail.com>
2022-05-13 21:54:03 +0800

    second refactor

--- Potter.py
+++ Potter.py
@@ -1,4 +1,7 @@
 class Potter:
+    def __init__(self,book_price):
+        self.book_price = book_price
+        self.discount = {0:1, 1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
     def pick_combination(self, basket, books_num):
         return list(set(basket))[:books_num]
     def remove_books_from_basket(self, basket, books):
@@ -6,10 +9,10 @@ class Potter:
             basket.remove(b)
         return basket
     def discountprice(self, basket):
-        if len(set(basket)) != len(basket):
+        type_num = len(basket)
+        if len(set(basket)) != type_num:
             print('Error: only accept unique basket.')
-        discount = {0:1, 1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
-        return 8*len(basket)*discount[len(basket)]
+        return self.book_price*type_num*self.discount[type_num]
     def price(self, basket):
         if len(set(basket)) == len(basket):
             return self.discountprice(basket)
--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import unittest
 from Potter import Potter
-
+book_price = 8
 class TestPotter(unittest.TestCase):
     def setUp(self):
-        self.test_object = Potter()
+        self.test_object = Potter(book_price)
     def testBasics(self):
         assert (0 == self.test_object.price([]))
-        assert (8 == self.test_object.price([1]))
-        assert (8 == self.test_object.price([2]))
-        assert (8 == self.test_object.price([3]))
-        assert (8 == self.test_object.price([4]))
-        assert (8 * 3 == self.test_object.price([1, 1, 1]))
+        assert (book_price == self.test_object.price([1]))
+        assert (book_price == self.test_object.price([2]))
+        assert (book_price == self.test_object.price([3]))
+        assert (book_price == self.test_object.price([4]))
+        assert (book_price * 3 == self.test_object.price([1, 1, 1]))
     def testSimpleDiscounts1(self):
-        assert (8 * 2 * 0.95 == self.test_object.price([0, 1]))
+        assert (book_price * 2 * 0.95 == self.test_object.price([0, 1]))
     def testSimpleDiscounts2(self):
-        assert (8 * 3 * 0.9 == self.test_object.price([0, 2, 4]))
+        assert (book_price * 3 * 0.9 == self.test_object.price([0, 2, 4]))
     def testSimpleDiscounts3(self):
-        assert (8 * 4 * 0.8 == self.test_object.price([0, 1, 2, 4]))
+        assert (book_price * 4 * 0.8 == self.test_object.price([0, 1, 2, 4]))
     def testSimpleDiscounts4(self):
-        assert (8 * 5 * 0.75 == self.test_object.price([0, 1, 2, 3, 4]))
+        assert (book_price * 5 * 0.75 == self.test_object.price([0, 1, 2, 3, 4]))
     def testSeveralDiscounts(self):
-        assert (8 + (8 * 2 * 0.95) == self.test_object.price([0, 0, 1]))
-        assert (2 * (8 * 2 * 0.95) == self.test_object.price([0, 0, 1, 1]))
-        assert ((8 * 4 * 0.8) + (8 * 2 * 0.95) == self.test_object.price([0, 0, 1, 2, 2, 3]))
-        assert (8 + (8 * 5 * 0.75) == self.test_object.price([0, 1, 1, 2, 3, 4]))
+        assert (book_price + (book_price * 2 * 0.95) == self.test_object.price([0, 0, 1]))
+        assert (2 * (book_price * 2 * 0.95) == self.test_object.price([0, 0, 1, 1]))
+        assert ((book_price * 4 * 0.8) + (book_price * 2 * 0.95) == self.test_object.price([0, 0, 1, 2, 2, 3]))
+        assert (book_price + (book_price * 5 * 0.75) == self.test_object.price([0, 1, 1, 2, 3, 4]))
 if __name__ == '__main__':
     unittest.main()
